a100a3fab8fd05ce8b1cee049c65c9ffd571fdf1
jdawud <jdawud@gmail.com>
2025-11-03 08:50:40 -0700

    Pre-test

---
+++ Froggy HopperTests/Froggy_HopperTests.swift
@@ -0,0 +1,16 @@
+//
+//  Froggy_HopperTests.swift
+//  Froggy HopperTests
+//
+//  Created by Junaid Dawud on 5/22/25.
+//
+
+import Testing
+
+struct Froggy_HopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
